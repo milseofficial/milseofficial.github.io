6137f8e3c482d96e5d8996cc0df31237fa72b1ba
milseofficial <119873085+milseofficial@users.noreply.github.com>
2022-12-05 21:29:02 +0300

    Update dynamics.js

--- dynamics.js
+++ dynamics.js
@@ -1,3 +1,4 @@
 function backplay101(){
   get('background-music').play();
 }
+get('milse-container').innerHTML='<div class="milse-container"><div class="milse-screen1"><div class="container"><audio id="background-music"><source src="background1.mp3" type="audio/mpeg"></audio><div class="row"><div class="col-sm-4"></div><div class="col-sm-4 padded-30" align="center"><img src="sample.png" class="img-responsive"><h1 onclick="backplay101()" class="screen1-text">Start Experience</h1></div><div class="col-sm-4"></div></div></div></div></div>'
